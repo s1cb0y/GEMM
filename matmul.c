fc74170de50451d0b7fb8b9253c890588279fd8d
Matthias Spandel <m.spandel86@gmail.com>
2023-12-30 08:26:54 +0100

    renamed block defines

--- matmul.c
+++ matmul.c
@@ -5,11 +5,13 @@
 #include <assert.h>
 #include <immintrin.h>
 #include <stdint.h>
-#include "matmulGPU.h"
+#include <string.h>
+//#include "matmulGPU.h"
 
 #define N 1024
-#define BLOCK_R 4
-#define BLOCK_C 2
+#define BLOCK_Y 4
+#define BLOCK_X 2
+#define BLOCK 8
 
 
 float A[N*N]; __attribute__ ((__aligned__((64))))
@@ -17,11 +19,9 @@ float B[N*N]; __attribute__ ((__aligned__((64))))
 float C[N*N]; __attribute__ ((__aligned__((64))))
 float val[N*N]; __attribute__ ((__aligned__((64))))
 
-
 __m256* Am = (__m256*) A;
 __m256* Bm = (__m256*) B;
 __m256* Cm = (__m256*) C;
-
 uint64_t nanos(){
     struct timespec time;
     clock_gettime(CLOCK_MONOTONIC_RAW, &time);           
@@ -52,21 +52,19 @@ void simpleMultiply(){
 // }
 
 const uint32_t TILE_SIZE = 4;
-#define BLOCK_Y 8
-#define BLOCK_X 2
-#define BLOCK_F 8
+
 // with tiling and
 void simpleMultiplyFast(){
     
     for (int t = 0; t < N; t+=TILE_SIZE){
         for (int r=0; r<N; r++) {
             for (int k=t; k<t+TILE_SIZE; k++) {
-                for (int c=0; c<N; c+=BLOCK_Y*BLOCK_F) {      
+                for (int c=0; c<N; c+=BLOCK_Y*BLOCK) {      
                     __m256 acc[BLOCK_Y] = {};                 
                     __m256 ar = _mm256_broadcast_ss(&A[(r)*N + k]);
                     for (int cb = 0; cb < BLOCK_Y; cb++){ // Force to use all 16 YMM registers
                         //acc[cb] += ar * B[k*N + c + cb];
-                        acc[cb] += _mm256_fmadd_ps(ar, Bm[((k*N + c + cb*BLOCK_F)/8)], acc[cb]);
+                        acc[cb] += _mm256_fmadd_ps(ar, Bm[((k*N + c + cb*BLOCK)/8)], acc[cb]);
                     }                      
                 
                     for (int cb = 0; cb < BLOCK_Y; cb++){ // store registers back to RAM
@@ -104,15 +102,14 @@ void simpleMultiplyFast(){
 // }
 //#else
 void multiplyBlocked(){
-    assert(N%BLOCK_R == 0);
-    assert(N%BLOCK_C == 0);
-    for (int rb = 0; rb < N; rb+=BLOCK_R){
-        for (int cb = 0; cb < N; cb+=BLOCK_C){
-             __m256 tc[BLOCK_R][BLOCK_C] = {};
+    assert(N%BLOCK_X == 0);
+    assert(N%BLOCK_Y == 0);
+    for (int rb = 0; rb < N; rb+=BLOCK_Y){
+        for (int cb = 0; cb < N; cb+=BLOCK_X){
+             __m256 tc[BLOCK_Y][BLOCK_X] = {};
             for (int k = 0; k < N; k += 8) {
-                for (int y = 0; y < BLOCK_R; y++) {
-                for (int x = 0; x < BLOCK_C; x++) {
-                    //printf("%d %d\n", ((rb+y)*N + k)/8, ((cb+x)*N + k)/8);
+                for (int y = 0; y < BLOCK_Y; y++) {
+                for (int x = 0; x < BLOCK_X; x++) {
                     tc[y][x] = _mm256_fmadd_ps(
                     Am[((rb+y)*N + k)/8],
                     Bm[((cb+x)*N + k)/8],
@@ -122,8 +119,8 @@ void multiplyBlocked(){
             }
 
             // store
-            for (int y = 0; y < BLOCK_R; y++) {
-                for (int x = 0; x < BLOCK_C; x++) {
+            for (int y = 0; y < BLOCK_Y; y++) {
+                for (int x = 0; x < BLOCK_X; x++) {
                 float ftmp = 0.0;
                 for (int i = 0; i < 8; i++) ftmp += tc[y][x][i];
                 C[(rb+y)*N + cb+x] = ftmp;
@@ -167,12 +164,12 @@ int main(){
             s = (end - start) * 1e-9;
             printf ("GFlops (naive optimized approach) / Time (s): %f, %f\n", flop*1e-9 / s, s);
             
-            // //blocked CPU multiply
-            // start = nanos();            
-            // multiplyBlocked();             
-            // end = nanos();
-            // s = (end - start) * 1e-9;
-            // printf ("GFlops (blocked approach): %f\n", flop*1e-9 / s);
+            //blocked CPU multiply
+            start = nanos();            
+            multiplyBlocked();             
+            end = nanos();
+            s = (end - start) * 1e-9;
+            printf ("GFlops (blocked approach): %f\n", flop*1e-9 / s);
             
             // memset(C, 0, N*N*sizeof(float));
             // // GPU multiply
